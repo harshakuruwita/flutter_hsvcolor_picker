6f5d9dd5a6df043ffcc5e15de0541ded25fa0cd2
Harsha Harischandra <harsha@virtualcapital.co.nz>
2020-10-06 14:05:17 +0530

    Final Chnage

--- lib/flutter_hsvcolor_picker.dart
+++ lib/flutter_hsvcolor_picker.dart
@@ -1214,6 +1214,7 @@ class _HexPickerState extends State<HexPicker> {
       //TextField
       new Expanded(
           child: new TextField(
+        maxLength: 6,
         style: Theme.of(context).textTheme.headline.copyWith(fontSize: 20),
         focusNode: new FocusNode()..addListener(() {}),
         controller: super.widget.controller,
